2658c5c24504402c7502071fd3cfc6f9f39fb31d
Robin Bourachot <rob1.bourachot@gmail.com>
2018-06-17 12:22:04 +0200

    Add Module Command (with unload option)

---
+++ core/commands/module.js
@@ -0,0 +1,23 @@
+exports.run = function(bot, message, args) {
+    if (args.length >= 2) {
+        if (args[0] == 'unload') {
+            args[1] = `./../${args[1]}`;
+            delete require.cache[require.resolve(`./${args[1]}.js`)];
+            message.channel.send(`The file **${args[1]}** has been unloaded, **${message.author.username}**.`);
+        }
+    } else {
+        message.channel.send(`You need to specify the action you want to perform and on what file, **${message.author.username}**.`);
+    }
+}
+
+exports.infos = {
+    name: "Module Management",
+    perms: {
+        bot: 1,
+        guild: 1,
+        discord: null
+    },
+    enabled: null,
+    category: "Admin",
+    description: "Manages the modules of the bot"
+}
