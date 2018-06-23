84331fb976690630a88c35e475b7e60877578418
Ciborn <rob1.bourachot@gmail.com>
2018-06-23 13:21:46 +0200

    Update Info Command

--- core/commands/info.js
+++ core/commands/info.js
@@ -1,12 +1,14 @@
 const Discord = require('discord.js');
 exports.run = function(bot, message, args) {
     const minutesUptime = `${String(Math.floor(bot.uptime%3600000/60000))}m`;
+    var users = 0;
+    for (let [guildId, data] of Object.entries(bot.cache.guilds)) users += Object.keys(data.members).length;
     const embed = new Discord.RichEmbed()
         .setAuthor(bot.user.username, bot.user.avatarURL)
         .setTitle('Informations on the Tranquility bot')
         .addField(`Developers`, `<@320933389513523220>\n<@310296184436817930>`, true)
-        .addField(`Statistics`, `**Users** : ${bot.users.size}\n**Servers** : ${bot.guilds.size}`, true)
-        .addField(`Ressources`, `**Memory Usage** : ${Math.round(100 * process.memoryUsage().heapTotal/1000000) / 100} MB\n**Uptime** : ${Math.floor(bot.uptime/3600000)}h${minutesUptime.length == 2 ? '0' + minutesUptime : minutesUptime}`, true)
+        .addField(`Statistics`, `**Users** : **${users}**/${bot.users.size}\n**Servers** : **${Object.keys(bot.cache.guilds).length}**/${bot.guilds.size}`, true)
+        .addField(`Resources`, `**Memory Usage** : ${Math.round(100 * process.memoryUsage().heapTotal/1000000) / 100} MB\n**Uptime** : ${Math.floor(bot.uptime/3600000)}h${minutesUptime.length == 2 ? '0' + minutesUptime : minutesUptime}`, true)
         .addField(`Host`, `**Raspberry Pi 3** at **Ciborn**'s home`, true)
         .addField(`Prefix in this guild`, `**${message.member.guild.prefix}**`, true)
         .setThumbnail(message.guild.iconURL)
